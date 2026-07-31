fb5e25ddf15e5730835c2bbb63c17421be05e6cd
Developer <dev@example.com>
2026-07-31 16:55:46 -0300

    Configure assetPrefix for GitHub Pages

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,15 @@
 /** @type {import('next').NextConfig} */
+const isProd = process.env.NODE_ENV === "production";
+
 const nextConfig = {
   output: "export",
-  basePath: "/portfolio",
+  basePath: isProd ? "/portfolio" : "",
+  assetPrefix: isProd ? "/portfolio/" : "",
   images: {
     unoptimized: true,
     formats: ["image/avif", "image/webp"],
   },
   reactStrictMode: true,
-}
+};
 
-export default nextConfig
+export default nextConfig;
